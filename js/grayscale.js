99909e7275c37a40333ae784d43738de5af24cb4
Luke Griffin <31658014+electroswagnetism@users.noreply.github.com>
2017-09-16 22:13:29 -0400

    updated map coordinates

--- js/grayscale.js
+++ js/grayscale.js
@@ -53,7 +53,7 @@ function init() {
     zoom: 15,
 
     // The latitude and longitude to center the map (always required)
-    center: new google.maps.LatLng(40.6700, -73.9400), // New York
+    center: new google.maps.LatLng(43.391489,-80.3987359), // New York
 
     // Disables the default Google Maps UI components
     disableDefaultUI: true,
